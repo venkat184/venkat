5b70b1841c9ad14197ee1ed3cee455b90546efc3
venkat184 <venkateshkonduru018@gmail.com>
2022-03-19 14:15:49 +0530

    project

---
+++ src/Com/simpiilearn/typecasting/Excerise2.java
@@ -0,0 +1,40 @@
+package Com.simpiilearn.typecasting;
+
+import java.util.Scanner;
+
+public class Excerise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s =new Scanner(System.in);
+		System.out.println("Enter the double number:");
+		double doublenumber =s.nextDouble();
+		int integernumber=(int)doublenumber;
+		short shortnumber=(short)doublenumber;
+		byte bytenumber=(byte)doublenumber;
+		float floatnumber=(float)doublenumber;
+		
+	System.out.println("integervalue:"+integernumber);
+	System.out.println("shortvalue:"+shortnumber);
+	System.out.println("bytevalue:"+bytenumber);
+	System.out.println("floatvalue:"+floatnumber);
+	System.out.println();
+	Scanner sc =new Scanner(System.in);
+	System.out.println("Enter the long number:");
+	long longnumber =sc.nextLong();
+	int integernumber1=(int)doublenumber;
+	short shortnumber1=(short)doublenumber;
+	byte bytenumber1=(byte)doublenumber;
+	float floatnumber1=(float)doublenumber;
+	
+System.out.println("integervalue:"+integernumber);
+System.out.println("shortvalue:"+shortnumber);
+System.out.println("bytevalue:"+bytenumber);
+System.out.println("floatvalue:"+floatnumber);
+
+
+
+
+	}
+
+}
